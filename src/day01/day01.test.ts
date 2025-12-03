ccafab78a0e75f6ae0c540f22432ab144383c6ec
James Sandy <james.sandy@arondite.com>
2025-12-03 00:01:14 +0000

    Day 1

--- src/day01/day01.test.ts
+++ src/day01/day01.test.ts
@@ -1,21 +1,68 @@
 import { describe, it, expect } from "vitest";
 import { part1, part2 } from "./index.js";
 
-describe("Day 01", () => {
+describe("test 1 - expect 6", () => {
   // Example input from the puzzle description
-  const sampleInput = [
-    "",
-  ];
-
-  describe("part1", () => {
-    it("should solve the sample input", () => {
-      expect(part1(sampleInput)).toBe(0); // Update with expected answer
-    });
-  });
+  const sampleInput = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
 
   describe("part2", () => {
     it("should solve the sample input", () => {
-      expect(part2(sampleInput)).toBe(0); // Update with expected answer
+      expect(part2(sampleInput)).toBe(6); // Update with expected answer
+    });
+  });
+});
+
+describe("test 2 - expect 4", () => {
+  // Example input from the puzzle description
+  const sampleInput = ["L50", "L1", "L1", "R2", "L2", "R2", "R100"];
+
+  describe("part2", () => {
+    it("should solve the sample input", () => {
+      expect(part2(sampleInput)).toBe(4); // Update with expected answer
+    });
+  });
+});
+
+describe("test 3 - expect 6", () => {
+  // Example input from the puzzle description
+  const sampleInput = ["L200", "R200", "L200"];
+
+  describe("part2", () => {
+    it("should solve the sample input", () => {
+      expect(part2(sampleInput)).toBe(6); // Update with expected answer
+    });
+  });
+});
+
+describe("test 4 - expect 3", () => {
+  // Example input from the puzzle description
+  const sampleInput = ["R50", "R100", "L100"];
+
+  describe("part2", () => {
+    it("should solve the sample input", () => {
+      expect(part2(sampleInput)).toBe(3); // Update with expected answer
+    });
+  });
+});
+
+describe("tet 5 - expect 4", () => {
+  // Example input from the puzzle description
+  const sampleInput = ["R50", "R101", "L102"];
+
+  describe("part2", () => {
+    it("should solve the sample input", () => {
+      expect(part2(sampleInput)).toBe(4); // Update with expected answer
+    });
+  });
+});
+
+describe("test 6 - expect 5", () => {
+  // Example input from the puzzle description
+  const sampleInput = ["R50", "R200", "L200"];
+
+  describe("part2", () => {
+    it("should solve the sample input", () => {
+      expect(part2(sampleInput)).toBe(5); // Update with expected answer
     });
   });
 });
--- src/day01/index.ts
+++ src/day01/index.ts
@@ -4,22 +4,83 @@
  * https://adventofcode.com/2025/day/1
  */
 
+interface Rotation {
+  direction_sign: number;
+  amount: number;
+}
+
 /**
  * Part 1:
  */
 export function part1(lines: string[]): number {
-  // Parse input
+  let rotations = getRotations(lines);
 
-  // TODO: Implement solution
-  return 0;
+  let current = 50;
+  let zero_count = 0;
+  for (const rotation of rotations) {
+    current += rotation.amount * rotation.direction_sign + 100;
+    current %= 100;
+    if (current == 0) {
+      zero_count += 1;
+    }
+  }
+
+  return zero_count;
 }
 
 /**
  * Part 2:
  */
 export function part2(lines: string[]): number {
-  // Parse input
+  let rotations = getRotations(lines);
 
-  // TODO: Implement solution
-  return 0;
+  let current = 50;
+  let zero_count = 0;
+  for (const rotation of rotations) {
+    let old_current = current;
+
+    let rotation_amount = rotation.amount;
+    if (rotation_amount >= 100) {
+      zero_count += Math.floor(rotation.amount / 100);
+    }
+    rotation_amount = rotation_amount % 100;
+
+    current += rotation_amount * rotation.direction_sign;
+
+    if (rotation_amount == 0) {
+      break;
+    } else if (current >= 100) {
+      zero_count += 1;
+    } else if (current <= 0 && old_current != 0) {
+      zero_count += 1;
+    }
+
+    current += 100;
+    current %= 100;
+  }
+
+  return zero_count;
+}
+
+function getRotations(lines: string[]): Rotation[] {
+  return lines.map((line) => {
+    const direction = line[0];
+    const amount = Number(line.slice(1));
+    switch (direction) {
+      case "R": {
+        return {
+          direction_sign: 1,
+          amount,
+        };
+      }
+      case "L": {
+        return {
+          direction_sign: -1,
+          amount,
+        };
+      }
+      default:
+        throw Error("Unexpected direction:" + direction);
+    }
+  });
 }
